e8b528bcee0ddfe09d7b9f8f91900b89d634b4c3
David Abram <david@crocoder.dev>
2023-09-11 00:15:26 +0200

    middleware

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
 import (
-  "context"
+	"context"
+	"net/http"
 
-  "github.com/labstack/echo/v4"
-  "github.com/donseba/go-htmx"
+	"github.com/donseba/go-htmx"
+	"github.com/labstack/echo/v4"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
 )
+
+type State struct {
+}
+
+type App struct {
+	htmx         *htmx.HTMX
+	appTemplates *Template
+
+	state State
+}
+
+func (a *App) Index(c echo.Context) error {
+  r := c.Request()
+  h := r.Context().Value(htmx.ContextRequestHeader).(htmx.HxRequestHeader)
+  println("HX-Boosted: ", h.HxBoosted)
+  println("HX-Current-URL: ", h.HxCurrentURL)
+  println("HX-History-Restore-Request: ", h.HxHistoryRestoreRequest)
+  println("HX-Prompt: ", h.HxPrompt)
+  println("HX-Request: ", h.HxRequest)
+  println("HX-Target: ", h.HxTarget)
+  println("HX-Trigger-Name: ", h.HxTriggerName)
+  println("HX-Trigger: ", h.HxTrigger)
+
+	return c.Render(http.StatusOK, "index.html", a.state)
+}
+
+func main() {
+	e := echo.New()
+	e.Use(echoMiddleware.Logger())
+	e.Use(echoMiddleware.Recover())
+	e.Use(HtmxMiddleware)
+
+	app := &App{
+		appTemplates: new(Template),
+    state: State{},
+	}
+
+	app.appTemplates.Add("templates/*.html")
+
+	e.Renderer = app.appTemplates
+
+	e.GET("/", app.Index)
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
 
 func HtmxMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
