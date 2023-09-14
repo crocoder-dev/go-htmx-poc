f5fe54c473a34ccf179c371186f6df65d7f5737f
David Abram <david@crocoder.dev>
2023-09-14 13:05:01 +0200

    embed stuff

--- templates.go
+++ templates.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"html/template"
 	"io"
 
@@ -11,12 +12,15 @@ type Template struct {
 	templates *template.Template
 }
 
+//go:embed templates/*
+var views embed.FS
+
 func (t *Template) Init() {
-  t.templates = template.New("")
+	t.templates = template.New("")
 }
 
 func (t *Template) Add(glob string) {
-  t.templates = template.Must(t.templates.ParseGlob(glob))
+	t.templates = template.Must(t.templates.ParseFS(views, glob))
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
